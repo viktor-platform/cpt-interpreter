331ffa1fbdd99293ba1e3924e694ab08a7bb3810
Raoul de Jonge <rdejonge@viktor.ai>
2024-11-01 17:00:05 +0100

    updated to newest sdk version

--- app/controller.py
+++ app/controller.py
@@ -16,20 +16,7 @@ SOFTWARE.
 """
 from pathlib import Path
 
-from viktor import File, UserError
-from viktor.core import ViktorController, progress_message
-from viktor.geo import GEFFile, SoilLayout
-from viktor.geometry import GeoPoint
-from viktor.result import DownloadResult, SetParametersResult
-from viktor.views import (
-    DataGroup,
-    DataItem,
-    MapPoint,
-    MapResult,
-    MapView,
-    PlotlyAndDataResult,
-    PlotlyAndDataView,
-)
+import viktor as vkt
 
 from .parametrization import Parametrization
 from .soil_layout_conversion_functions import (
@@ -41,84 +28,84 @@ from .soil_layout_conversion_functions import (
 from .visualisation import visualise_cpt
 
 
-class CPTFileController(ViktorController):
+class CPTFileController(vkt.ViktorController):
     label = "CPT File"
     parametrization = Parametrization(width=40)
 
-    def classify_soil_layout(self, params, **kwargs) -> SetParametersResult:
+    def classify_soil_layout(self, params, **kwargs) -> vkt.SetParamsResult:
         """Classify the CPT file when it is first uploaded"""
         if params.classification.get_sample_gef_toggle:
             file = self._get_sample_gef_file()
         else:
             file_resource = params.classification.gef_file
             if not file_resource:
-                raise UserError("Upload and select a GEF file.")
+                raise vkt.UserError("Upload and select a GEF file.")
             file = file_resource.file
 
-        cpt_file = GEFFile(file.getvalue("ISO-8859-1"))
+        cpt_file = vkt.geo.GEFFile(file.getvalue("ISO-8859-1"))
         classification = Classification(params["classification"])
         results = classification.classify_cpt_file(cpt_file)
 
-        return SetParametersResult(results)
+        return vkt.SetParamsResult(results)
 
     @staticmethod
     def _get_sample_gef_file():
         gef_file_path = Path(__file__).parent / "sample_gef.GEF"
-        return File.from_path(gef_file_path)
+        return vkt.File.from_path(gef_file_path)
 
     def download_sample_gef_file(self, **kwargs):
         """Download the sample GEF file."""
         gef_file = self._get_sample_gef_file()
-        return DownloadResult(gef_file, "sample_gef.GEF")
+        return vkt.DownloadResult(gef_file, "sample_gef.GEF")
 
-    @PlotlyAndDataView("CPT interpretation", duration_guess=3)
-    def visualize_cpt(self, params, **kwargs) -> PlotlyAndDataResult:
+    @vkt.PlotlyAndDataView("CPT interpretation", duration_guess=3)
+    def visualize_cpt(self, params, **kwargs) -> vkt.PlotlyAndDataResult:
         """Visualizes the Qc and Rf line plots, the soil layout bar plots and the data of the cpt."""
         fig = visualise_cpt(cpt_params=params)
         data_group = self.get_data_group(params)
-        return PlotlyAndDataResult(fig.to_json(), data=data_group)
+        return vkt.PlotlyAndDataResult(fig.to_json(), data=data_group)
 
-    @MapView("Map", duration_guess=2)
-    def visualize_map(self, params, **kwargs) -> MapResult:
+    @vkt.MapView("Map", duration_guess=2)
+    def visualize_map(self, params, **kwargs) -> vkt.MapResult:
         """Visualize the MapView with the CPT location."""
         headers = params.get("headers")
         if not headers:
-            raise UserError("GEF file has no headers")
+            raise vkt.UserError("GEF file has no headers")
 
         x_coordinate, y_coordinate = headers.x_y_coordinates
         cpt_features = []
         if None not in (x_coordinate, y_coordinate):
-            cpt_features.append(MapPoint.from_geo_point(GeoPoint.from_rd((x_coordinate, y_coordinate))))
-        return MapResult(cpt_features)
+            cpt_features.append(vkt.MapPoint.from_geo_point(vkt.GeoPoint.from_rd((x_coordinate, y_coordinate))))
+        return vkt.MapResult(cpt_features)
 
     @staticmethod
-    def get_data_group(params) -> DataGroup:
+    def get_data_group(params) -> vkt.DataGroup:
         """Collect the necessary information from the GEF headers and return a DataGroup with the data"""
         headers = params.get("headers")
         if not headers:
-            raise UserError("GEF file has no headers")
+            raise vkt.UserError("GEF file has no headers")
 
         x_coordinate, y_coordinate = headers.x_y_coordinates
-        return DataGroup(
-            ground_level_wrt_reference_m=DataItem(
+        return vkt.DataGroup(
+            ground_level_wrt_reference_m=vkt.DataItem(
                 "Ground level", headers.ground_level_wrt_reference_m or -999, suffix="m"
             ),
-            ground_water_level=DataItem("Phreatic level", params.ground_water_level, suffix="m"),
-            height_system=DataItem("Height system", headers.height_system or "-"),
-            coordinates=DataItem(
+            ground_water_level=vkt.DataItem("Phreatic level", params.ground_water_level, suffix="m"),
+            height_system=vkt.DataItem("Height system", headers.height_system or "-"),
+            coordinates=vkt.DataItem(
                 "Coordinates",
                 "",
-                subgroup=DataGroup(
-                    x_coordinate=DataItem("X-coordinate", x_coordinate or 0, suffix="m"),
-                    y_coordinate=DataItem("Y-coordinate", y_coordinate or 0, suffix="m"),
+                subgroup=vkt.DataGroup(
+                    x_coordinate=vkt.DataItem("X-coordinate", x_coordinate or 0, suffix="m"),
+                    y_coordinate=vkt.DataItem("Y-coordinate", y_coordinate or 0, suffix="m"),
                 ),
             ),
         )
 
     @staticmethod
-    def filter_soil_layout_on_min_layer_thickness(params, **kwargs) -> SetParametersResult:
+    def filter_soil_layout_on_min_layer_thickness(params, **kwargs) -> vkt.SetParamsResult:
         """Remove all layers below the filter threshold."""
-        progress_message("Filtering thin layers from soil layout")
+        vkt.progress_message("Filtering thin layers from soil layout")
 
         # Create SoilLayout
         classification = Classification(params.classification)
@@ -137,22 +124,22 @@ class CPTFileController(ViktorController):
         soil_layout_user = convert_soil_layout_from_mm_to_meter(soil_layout_user)
         table_input_soil_layers = convert_soil_layout_to_input_table_field(soil_layout_user)
 
-        return SetParametersResult({"soil_layout": table_input_soil_layers})
+        return vkt.SetParamsResult({"soil_layout": table_input_soil_layers})
 
     @staticmethod
-    def reset_soil_layout_user(params, **kwargs) -> SetParametersResult:
+    def reset_soil_layout_user(params, **kwargs) -> vkt.SetParamsResult:
         """Place the original soil layout (after parsing) in the table input."""
-        progress_message("Resetting soil layout to original unfiltered result")
+        vkt.progress_message("Resetting soil layout to original unfiltered result")
 
         # get the original soil layout from the hidden field
-        soil_layout_original = SoilLayout.from_dict(params.soil_layout_original)
+        soil_layout_original = vkt.geo.SoilLayout.from_dict(params.soil_layout_original)
 
         # convert it to a format for the input table
         table_input_soil_layers = convert_soil_layout_to_input_table_field(
             convert_soil_layout_from_mm_to_meter(soil_layout_original)
         )
 
-        return SetParametersResult(
+        return vkt.SetParamsResult(
             {
                 "soil_layout": table_input_soil_layers,
                 "bottom_of_soil_layout_user": params.get("bottom_of_soil_layout_user"),
--- app/soil_layout_conversion_functions.py
+++ app/soil_layout_conversion_functions.py
@@ -18,37 +18,27 @@ from copy import deepcopy
 from math import ceil
 from typing import List, Union
 
-from viktor import Color, UserError
-from viktor.geo import (
-    GEFClassificationError,
-    GEFFile,
-    GEFParsingException,
-    RobertsonMethod,
-    Soil,
-    SoilLayer,
-    SoilLayout,
-    TableMethod,
-)
+import viktor as vkt
 
 from .constants import ADDITIONAL_COLUMNS, DEFAULT_MIN_LAYER_THICKNESS
 
 
-def convert_soil_layout_from_mm_to_meter(soil_layout: SoilLayout) -> SoilLayout:
+def convert_soil_layout_from_mm_to_meter(soil_layout: vkt.geo.SoilLayout) -> vkt.geo.SoilLayout:
     """Converts the units of the SoilLayout from mm to m."""
     serialization_dict = soil_layout.serialize()
     for layer in serialization_dict["layers"]:
         layer["top_of_layer"] = layer["top_of_layer"] / 1000
         layer["bottom_of_layer"] = layer["bottom_of_layer"] / 1000
-    return SoilLayout.from_dict(serialization_dict)
+    return vkt.geo.SoilLayout.from_dict(serialization_dict)
 
 
-def convert_soil_layout_from_meter_to_mm(soil_layout: SoilLayout) -> SoilLayout:
+def convert_soil_layout_from_meter_to_mm(soil_layout: vkt.geo.SoilLayout) -> vkt.geo.SoilLayout:
     """Converts the units of the SoilLayout from m to mm."""
     serialization_dict = soil_layout.serialize()
     for layer in serialization_dict["layers"]:
         layer["top_of_layer"] = layer["top_of_layer"] * 1000
         layer["bottom_of_layer"] = layer["bottom_of_layer"] * 1000
-    return SoilLayout.from_dict(serialization_dict)
+    return vkt.geo.SoilLayout.from_dict(serialization_dict)
 
 
 def filter_nones_from_params_dict(raw_dict) -> dict:
@@ -67,7 +57,7 @@ def convert_input_table_field_to_soil_layout(
     bottom_of_soil_layout_user: float,
     soil_layers_from_table_input: List[dict],
     soils: dict,
-) -> SoilLayout:
+) -> vkt.geo.SoilLayout:
     """Creates a SoilLayout from the user input.
 
     :param bottom_of_soil_layout_user: Bottom of soil layout in [m]
@@ -82,28 +72,28 @@ def convert_input_table_field_to_soil_layout(
         soil_name = layer["name"]
         top_of_layer = layer["top_of_layer"]
         try:
-            soil_layers.append(SoilLayer(soils[soil_name], top_of_layer, bottom))
+            soil_layers.append(vkt.geo.SoilLayer(soils[soil_name], top_of_layer, bottom))
         except KeyError:
-            raise UserError(
+            raise vkt.UserError(
                 f"'{soil_name}' is not available in the selected classification table.\n"
                 f"Please select a different table, or reclassify the CPT files"
             )
         bottom = top_of_layer  # Set bottom of next soil layer to top of current layer.
-    return convert_soil_layout_from_meter_to_mm(SoilLayout(soil_layers[::-1]))
+    return convert_soil_layout_from_meter_to_mm(vkt.geo.SoilLayout(soil_layers[::-1]))
 
 
-def convert_soil_layout_to_input_table_field(soil_layout: SoilLayout) -> List[dict]:
+def convert_soil_layout_to_input_table_field(soil_layout: vkt.geo.SoilLayout) -> List[dict]:
     """Converts a SoilLayout to the parametrization representation (Field = InputTable)."""
     return [
         {"name": layer.soil.properties.ui_name, "top_of_layer": layer.top_of_layer} for layer in soil_layout.layers
     ]
 
 
-def convert_to_color(rgb: Union[str, tuple]) -> Color:
+def convert_to_color(rgb: Union[str, tuple]) -> vkt.Color:
     """Simple conversion function that always returns a Color object"""
     if isinstance(rgb, tuple):
-        return Color(*rgb)
-    return Color(*[int(element) for element in rgb.strip().split(",")])
+        return vkt.Color(*rgb)
+    return vkt.Color(*[int(element) for element in rgb.strip().split(",")])
 
 
 def get_water_level(cpt_data_object) -> float:
@@ -130,7 +120,7 @@ def _update_classification_table(classification_table: List[dict]) -> List[dict]
 def _update_color_string(classification_table: List[dict]) -> List[dict]:
     """Converts the RGB color strings in the table into a tuple (R, G, B)"""
     for row in classification_table:
-        if not isinstance(row["color"], Color):
+        if not isinstance(row["color"], vkt.Color):
             row["color"] = convert_to_color(row["color"])
     return classification_table
 
@@ -152,13 +142,13 @@ class Classification:
             return _update_color_string(self._table)
         return _update_classification_table(self._table)
 
-    def method(self, ground_water_level) -> Union[TableMethod, RobertsonMethod]:
+    def method(self, ground_water_level) -> Union[vkt.geo.TableMethod, vkt.geo.RobertsonMethod]:
         """Returns the appropriate _ClassificationMethod for the CPTData.classify() function"""
         if self._method == "robertson":
-            return RobertsonMethod(self.table)
+            return vkt.geo.RobertsonMethod(self.table)
         if self._method == "table":
-            return TableMethod(self.table, ground_water_level=ground_water_level)
-        raise UserError(f"The {self._method} method has not yet been implemented")
+            return vkt.geo.TableMethod(self.table, ground_water_level=ground_water_level)
+        raise vkt.UserError(f"The {self._method} method has not yet been implemented")
 
     @property
     def soil_mapping(self) -> dict:
@@ -169,10 +159,10 @@ class Classification:
             properties = deepcopy(soil)
             if self._method == "robertson":
                 del properties["color"]
-            soil_mapping[ui_name] = Soil(soil["name"], convert_to_color(soil["color"]), properties=properties)
+            soil_mapping[ui_name] = vkt.geo.Soil(soil["name"], convert_to_color(soil["color"]), properties=properties)
         return soil_mapping
 
-    def classify_cpt_file(self, cpt_file: GEFFile, saved_ground_water_level=None) -> dict:
+    def classify_cpt_file(self, cpt_file: vkt.geo.GEFFile, saved_ground_water_level=None) -> dict:
         """Classify an uploaded CPT File based on the selected _ClassificationMethod"""
 
         try:
@@ -190,10 +180,10 @@ class Classification:
                 method=self.method(ground_water_level), return_soil_layout_obj=True
             )
 
-        except GEFParsingException as e:
-            raise UserError(f"CPT Parsing: {str(e)}")
-        except GEFClassificationError as e:
-            raise UserError(f"CPT Classification: {str(e)}")
+        except vkt.GEFParsingException as e:
+            raise vkt.UserError(f"CPT Parsing: {str(e)}")
+        except vkt.GEFClassificationError as e:
+            raise vkt.UserError(f"CPT Classification: {str(e)}")
 
         soil_layout_filtered = soil_layout_obj.filter_layers_on_thickness(
             min_layer_thickness=DEFAULT_MIN_LAYER_THICKNESS,
